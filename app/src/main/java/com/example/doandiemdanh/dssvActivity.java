c3e1bd9b9e0f1611ba27df12ac11c47d4d4acc06
nhathang99 <58796854+nhathang99@users.noreply.github.com>
2020-07-10 00:50:02 +0700

    export file

--- app/src/main/java/com/example/doandiemdanh/dssvActivity.java
+++ app/src/main/java/com/example/doandiemdanh/dssvActivity.java
@@ -2,13 +2,17 @@ package com.example.doandiemdanh;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +23,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class dssvActivity extends AppCompatActivity {
@@ -33,7 +39,9 @@ public class dssvActivity extends AppCompatActivity {
     User users;
     user_class User_class;
     courses Courses;
-    String ten;
+    String ten, lop;
+    Button csv;
+    StringBuilder data;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
@@ -42,7 +50,7 @@ public class dssvActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dssv);
         tensv = findViewById(R.id.txt_tensv);
         //lấy mã môn
-
+        csv = findViewById(R.id.btncsv);
 
         Courses = new courses();
         User_class = new user_class();
@@ -55,6 +63,7 @@ public class dssvActivity extends AppCompatActivity {
         list = new ArrayList<>();
         list1 = new ArrayList<>();
         list1 = getIntent().getExtras().getStringArrayList("uid");
+        lop = getIntent().getExtras().get("class").toString();
         adapter = new ArrayAdapter<String>(this,R.layout.dssv_info,R.id.txt_tensv,list);
 
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
@@ -66,10 +75,62 @@ public class dssvActivity extends AppCompatActivity {
                     User user = ds.getValue(User.class);
                     if (list1.contains(ds.getKey())){
                         String a = user.getFullName();
-                        list.add(a);
+
+                        list.add(user.getMs()+" "+a);
                     }
                 }
                 listView.setAdapter(adapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
+        csv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                export(view);
+            }
+        });
+    }
+    public void export (View view){
+        //generate data
+        data = new StringBuilder();
+        data.append(lop+"\n");
+        data.append("Mssv,Name");
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userRef = rootRef.child("user");
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    User user = ds.getValue(User.class);
+                    if (list1.contains(ds.getKey())){
+                        String a = user.getMs();
+                        data.append("\n" + a + "," + user.getFullName());
+                    }
+                }
+                try {
+                    //saving the file into device
+                    FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
+                    out.write((data.toString()).getBytes());
+                    out.close();
+
+                    //exporting
+                    Context context = getApplicationContext();
+                    File filelocation = new File(getFilesDir(), "data.csv");
+                    Uri path = FileProvider.getUriForFile(context, "com.example.doandiemdanh.fileprovider", filelocation);
+                    Intent fileIntent = new Intent(Intent.ACTION_SEND);
+                    fileIntent.setType("text/csv");
+                    fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+                    startActivity(Intent.createChooser(fileIntent, "Send mail"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
